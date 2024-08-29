ae791221094b39d0d9594598a7e54e94a5effa95
Toshihiro Yagi <sylc.yagi@gmail.com>
2024-08-29 17:19:19 +0900

    add excludedFile.json add base branch option add origin prefix option

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import { exec } from "child_process";
+import { exec } from "node:child_process";
+import * as fs from "node:fs";
 
 const targetBranch = process.argv[2];
 
@@ -8,11 +9,30 @@ if (!targetBranch) {
 	console.error("Usage: prdiff <target_branch>");
 	process.exit(1);
 }
-const excludedFiles = ["package-lock.json", ".env"];
-const excludeArgs = excludedFiles.map((file) => `:(exclude)${file}`).join(" ");
 
-// git diffコマンドを実行
-const command = `git diff origin/main...origin/${targetBranch} -- ${excludeArgs}`;
+const baseBranch = process.argv[3] || "origin/main";
+const useOriginPrefix = process.argv[4] !== "false";
+
+const excludedFilesPath = "./excludedFiles.json";
+let excludedFiles: string[] = [];
+try {
+	const data = fs.readFileSync(excludedFilesPath, "utf8");
+	const json = JSON.parse(data);
+	excludedFiles = json.excludedFiles || [];
+} catch (error) {
+	console.error(`Error reading excluded files: ${(error as Error).message}`);
+	process.exit(1);
+}
+
+const excludeArgs = excludedFiles
+	.map((file) => `':(exclude)${file}'`)
+	.join(" ");
+
+const targetBranchWithPrefix = useOriginPrefix
+	? `origin/${targetBranch}`
+	: targetBranch;
+
+const command = `git diff ${baseBranch}...origin/${targetBranchWithPrefix} -- ${excludeArgs}`;
 
 exec(command, (error, stdout, stderr) => {
 	if (error) {
